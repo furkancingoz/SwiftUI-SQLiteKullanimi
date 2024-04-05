a200a3a9916f7083bd5f1ebd42dd2673d717779f
Furkan Cingöz <efcingoz@outlook.com>
2024-04-06 01:14:49 +0300

    Initial Commit

---
+++ SQLiteKullanimi/SQLiteKullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteKullanimiApp.swift
+//  SQLiteKullanimi
+//
+//  Created by Furkan Cingöz on 6.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteKullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
